f8c58b10beb5cefa83f89bc6fcd42d8dd0f7f726
g1tha <jeewantha.k@gmail.com>
2022-04-26 00:01:45 +1000

    Completed layout and main sketch functionality.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 const gridContainer = document.querySelector('.grid-container');
 const slider = document.querySelector('.slider');
 const slideLabel = document.querySelector('.slide-label');
-
+const colorPicker = document.querySelector('.color');
+let gridItems = Array.from(document.querySelectorAll('.grid-item'));
+let mousedownState = false;
 
 let gridSize = 2;
 slider.addEventListener('change', updateSlider);
+colorPicker.addEventListener('change', updateColor);
+document.addEventListener('mousedown', mousedownFunction);
+document.addEventListener('mouseup', mouseupFunction);
+let pickedColor = '#23A4B0';
 makeGrid(gridSize);
+
 
 function makeGrid(n) {
     while (gridContainer.firstChild) {
@@ -17,6 +24,9 @@ function makeGrid(n) {
         gridContainer.appendChild(gridItem);
     }
     gridContainer.style.gridTemplateColumns = `repeat(${n}, auto)`;
+    gridItems = Array.from(document.querySelectorAll('.grid-item'));
+    gridItems.forEach(e => e.addEventListener('mouseover', drawOver));
+    gridItems.forEach(e => e.addEventListener('click', draw));
 }
 
 function updateSlider() {
@@ -25,4 +35,25 @@ function updateSlider() {
     makeGrid(gridSize);
 }
 
+function updateColor() {
+    pickedColor = colorPicker.value;
+}
+
+function drawOver(e) {
+    if (mousedownState === true) {
+         e.target.style.backgroundColor = pickedColor;
+    }
+}
+
+function draw(e) {
+    e.target.style.backgroundColor = pickedColor;
+}
+
+function mousedownFunction() {
+    mousedownState = true;
+}
+
+function mouseupFunction() {
+    mousedownState = false;
+}
 
